d838df87745a52488db659b0483d445ab3514689
Vipolian <polevit@ya.ru>
2016-10-23 18:55:33 +0300

    Update Stack.hpp

--- include/Stack.hpp
+++ include/Stack.hpp
@@ -35,7 +35,7 @@ public:
 	const T& top();
 	stack & operator=(const stack &obj);
 	bool operator==(stack const & rhs);
-	
+	bool empty() const noexcept;/*noexcept*/
 };
 #include "Stack.cpp"
 #endif
